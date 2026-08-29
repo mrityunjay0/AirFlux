5766fa480c27c789f29136a2ad776a9df1ac56bb
Mrityunjay Kumar <mrityunjaykmr.mk@gmail.com>
2026-08-29 13:58:05 +0530

    Airport CRUD started under locationService

---
+++ common-lib/src/main/java/com/airflux/payload/embeddable/Address.java
@@ -0,0 +1,18 @@
+package com.airflux.payload.embeddable;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Embeddable
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+
+    private String street;
+    private String postalCode;
+}
---
+++ services/locationService/src/main/java/com/airflux/locationService/entity/Airport.java
@@ -0,0 +1,105 @@
+package com.airflux.locationService.entity;
+
+import com.airflux.payload.embeddable.Address;
+import com.airflux.payload.embeddable.GeoCode;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Builder;
+
+@Entity
+@Builder
+public class Airport {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false, length = 3)
+    private String iataCode;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @Embedded
+    private GeoCode geoCode;
+
+    @Column(name = "time_zone_id", length = 50)
+    private String timeZoneId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonIgnore
+    private City city;
+
+
+    public Airport() {
+    }
+
+    public Airport(Long id, String iataCode, String name, Address address, GeoCode geoCode, String timeZoneId, City city) {
+        this.id = id;
+        this.iataCode = iataCode;
+        this.name = name;
+        this.address = address;
+        this.geoCode = geoCode;
+        this.timeZoneId = timeZoneId;
+        this.city = city;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getIataCode() {
+        return iataCode;
+    }
+
+    public void setIataCode(String iataCode) {
+        this.iataCode = iataCode;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public GeoCode getGeoCode() {
+        return geoCode;
+    }
+
+    public void setGeoCode(GeoCode geoCode) {
+        this.geoCode = geoCode;
+    }
+
+    public String getTimeZoneId() {
+        return timeZoneId;
+    }
+
+    public void setTimeZoneId(String timeZoneId) {
+        this.timeZoneId = timeZoneId;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
+}
