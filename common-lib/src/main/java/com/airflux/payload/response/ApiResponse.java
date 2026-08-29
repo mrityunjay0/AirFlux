090911293a24526d1bbc4f3d7d66ea72e0f77e44
Mrityunjay Kumar <mrityunjaykmr.mk@gmail.com>
2026-08-29 12:34:00 +0530

    locationService CRUD completed and other controller features started like search etc.

---
+++ common-lib/src/main/java/com/airflux/payload/response/ApiResponse.java
@@ -0,0 +1,12 @@
+package com.airflux.payload.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse {
+    private String message;
+}
--- services/locationService/src/main/java/com/airflux/locationService/controller/CityController.java
+++ services/locationService/src/main/java/com/airflux/locationService/controller/CityController.java
@@ -2,8 +2,13 @@ package com.airflux.locationService.controller;
 
 import com.airflux.locationService.service.CityService;
 import com.airflux.payload.request.CityRequest;
+import com.airflux.payload.response.ApiResponse;
 import com.airflux.payload.response.CityResponse;
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -18,16 +23,55 @@ public class CityController {
     }
 
 
+    // CREATE CITY
     @PostMapping("/createCity")
     public ResponseEntity<CityResponse> createCity(@Valid @RequestBody CityRequest cityRequest) throws Exception {
         CityResponse createdCity = cityService.createCity(cityRequest);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdCity);
     }
 
+
+    // GET CITY BY ID
     @GetMapping("/{cityId}")
     public ResponseEntity<CityResponse> getCityById(@PathVariable Long cityId) throws Exception {
         CityResponse cityResponse = cityService.getCityById(cityId);
         return ResponseEntity.status(HttpStatus.OK).body(cityResponse);
+    }
+
+
+    // GET ALL CITIES (INTO PAGE FORMATE)
+    @GetMapping("/getAll")
+    public ResponseEntity<Page<CityResponse>> getAllCity(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "20") int size,
+            @RequestParam(defaultValue = "name") String sortBy,
+            @RequestParam(defaultValue = "asc") String sortDirection
+            ) throws Exception {
+
+        Sort sort = Sort.by(Sort.Direction.fromString(sortDirection), sortBy);
+        Pageable pageable = PageRequest.of(page, size, sort);
+        Page<CityResponse> responses = cityService.getAllCities(pageable);
+        return ResponseEntity.status(HttpStatus.OK).body(responses);
+
+    }
+
+
+    // UPDATE CITY
+    @PutMapping("/update/{cityId}")
+    public ResponseEntity<CityResponse> updateCity(@PathVariable Long cityId, @Valid @RequestBody CityRequest cityRequest) throws Exception {
+
+        CityResponse updatedCity = cityService.updateCity(cityId, cityRequest);
+        return ResponseEntity.status(HttpStatus.OK).body(updatedCity);
+    }
+
+
+    // DELETE CITY
+    @DeleteMapping("/delete/{cityId}")
+    public ResponseEntity<ApiResponse> deleteCity(@PathVariable Long cityId) throws Exception {
+
+        cityService.deleteCity(cityId);
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new ApiResponse("City deleted successfully"));
     }
 
     
