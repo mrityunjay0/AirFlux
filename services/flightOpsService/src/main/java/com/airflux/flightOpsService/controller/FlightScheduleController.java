26e1b934a2faacac8576dfcd5ff90dacc3881d08
Mrityunjay Kumar <mrityunjaykmr.mk@gmail.com>
2026-09-22 23:54:46 +0530

    Flight instance controller completed

--- services/flightOpsService/src/main/java/com/airflux/flightOpsService/controller/FlightScheduleController.java
+++ services/flightOpsService/src/main/java/com/airflux/flightOpsService/controller/FlightScheduleController.java
@@ -1,8 +1,13 @@
 package com.airflux.flightOpsService.controller;
 
 import com.airflux.flightOpsService.service.FlightScheduleService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.airflux.payload.request.FlightScheduleRequest;
+import com.airflux.payload.response.ApiResponse;
+import com.airflux.payload.response.FlightScheduleResponse;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/flight-schedules")
@@ -15,4 +20,47 @@ public class FlightScheduleController {
     }
 
 
+    @PostMapping
+    public ResponseEntity<FlightScheduleResponse> createFlightSchedule(
+            @RequestHeader("X-Airline-Id") Long airlineId,
+            @Valid @RequestBody FlightScheduleRequest flightScheduleRequest) {
+
+//        todo: watch airlineId
+        return ResponseEntity.status(HttpStatus.CREATED)
+                .body(flightScheduleService.createFlightSchedule(airlineId, flightScheduleRequest));
+    }
+
+    @GetMapping("/{airlineId}")
+    public ResponseEntity<?> getFlightScheduleByAirlineId(@RequestHeader("X-Airline-Id") Long airlineId) {
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(flightScheduleService.getFlightScheduleByAirline(airlineId));
+    }
+
+    @GetMapping("/{flightScheduleId}")
+    public ResponseEntity<FlightScheduleResponse> getFlightScheduleById(@PathVariable("flightScheduleId") Long flightScheduleId) {
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(flightScheduleService.getFlightScheduleById(flightScheduleId));
+    }
+
+    @PutMapping("/{flightScheduleId}")
+    public ResponseEntity<FlightScheduleResponse> updateFlightSchedule(@PathVariable Long flightScheduleId,
+                                                                       @Valid @RequestBody FlightScheduleRequest flightScheduleRequest) {
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(flightScheduleService.updateFlightSchedule(flightScheduleId, flightScheduleRequest));
+    }
+
+    @DeleteMapping("/{flightScheduleId}")
+    public ResponseEntity<ApiResponse> deleteFlightSchedule(@PathVariable Long flightScheduleId) {
+
+        flightScheduleService.deleteFlightScheduleById(flightScheduleId);
+
+        ApiResponse apiResponse = new ApiResponse();
+        apiResponse.setMessage("Successfully deleted FlightSchedule");
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(apiResponse);
+    }
 }
